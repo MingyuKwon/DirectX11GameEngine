17012731f93d51124e9e86ca23d83fdd0caaa1eb
Kwon Mingyu <kmgmg230601@gmail.com>
2025-07-07 00:39:40 +0900

    노말 구할 때는, 이동 부분을 제외한 회전 부분만 뽑아서 계산하도록 했다. 이제 원점이 아니더라도 정상 작동한다

--- KMGEngine.cpp
+++ KMGEngine.cpp
@@ -44,7 +44,7 @@ void ChangeCubeTransform()
 {
     if (schedular)
     {
-        //schedular->PushCommand(KMGCommand::RotateActor(L"Actor1", XMVectorSet(0, deltaTime, 0, 0)));
+        schedular->PushCommand(KMGCommand::RotateActor(L"Actor2", XMVectorSet(0, deltaTime, 0, 0)));
         float scale = 0.3f;
         schedular->PushCommand(KMGCommand::UpdateActorScale(L"Actor1", XMVectorSet(scale, scale, scale, 0)));
 
@@ -195,6 +195,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
                     schedular->PushCommand(KMGCommand::UpdateActorPosition(L"Actor1", XMVectorSet(4.0f, 0.0f, 0.0f, 0.0f)));
 
                     schedular->PushCommand(KMGCommand::AddActor(L"Actor2"));
+                    schedular->PushCommand(KMGCommand::UpdateActorPosition(L"Actor2", XMVectorSet(-1.0f, 0.0f, 0.0f, 0.0f)));
 
                     schedular->PushCommand(KMGCommand::AddActor(L"Light1"));
                     schedular->PushCommand(KMGCommand::UpdateActorPosition(L"Light1", XMVectorSet(2.0f, 0.0f, 0.0f, 0.0f)));
@@ -208,7 +209,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
             {
                 if (schedular)
                 {
-                    schedular->PushCommand(KMGCommand::UpdateLightComponent_Intensity(L"Light1", 2));
+                    schedular->PushCommand(KMGCommand::UpdateLightComponent_Intensity(L"Light1", 1.3));
 
                     schedular->PushCommand(KMGCommand::AddStaticMeshComponent(L"Actor1"));
                     schedular->PushCommand(KMGCommand::UpdateStaticMesh(L"Actor1", "Resource\\TankModel.obj",L"Resource\\Texture\\centurion.dds"));
--- KMGLib/VertexShader.hlsli
+++ KMGLib/VertexShader.hlsli
@@ -5,24 +5,21 @@ PS_INPUT VS(VS_INPUT input)
     PS_INPUT output = (PS_INPUT) 0;
 
     float4 worldPos = mul(input.Pos, World);
-    float4 worldNormal = normalize(mul(input.Normal, World));
-    float4 worldTangent = normalize(mul(input.Tangent, World));
-    float4 worldBinormal = normalize(mul(input.Binormal, World));
+
+    float3x3 worldRot = (float3x3) World;
+
+    float3 worldNormal = normalize(mul(input.Normal.xyz, worldRot));
+    float3 worldTangent = normalize(mul(input.Tangent.xyz, worldRot));
+    float3 worldBinormal = normalize(mul(input.Binormal.xyz, worldRot));
 
     output.worldPos = worldPos;
     output.Pos = mul(worldPos, View);
     output.Pos = mul(output.Pos, Projection);
     output.Tex = input.Tex;
 
-    output.Normal = worldNormal;
-    output.Tangent = worldTangent;
-    output.Binormal = worldBinormal;
-    
-    //output.Normal = input.Normal;
-    //output.Tangent = input.Tangent;
-    //output.Binormal = input.Binormal;
-
-    
+    output.Normal = float4(worldNormal, 0.0);
+    output.Tangent = float4(worldTangent, 0.0);
+    output.Binormal = float4(worldBinormal, 0.0);
     output.Color = input.Color;
 
     return output;
